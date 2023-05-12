3e73f506b8aac1a2a587da1bf27c6bda511bd5ed
pandasoli <sadpandasoli@gmail.com>
2023-05-12 20:15:54 -0300

    feat: return what positions the pattern were found at

--- split_string.go
+++ split_string.go
@@ -3,15 +3,16 @@ package colorstrings
 import . "github.com/pandasoli/colorstrings/utils"
 
 
-func (self ColorString) SplitString(at string) (res []ColorString) {
+func (self ColorString) SplitString(at string) (res []ColorString, pos []uint) {
   for _, raw_part := range Split(self.string_, at) {
     part, _ := self.Substring(
       raw_part.Pos,
       uint(len(raw_part.String)),
     )
 
+    pos = append(pos, raw_part.Pos)
     res = append(res, part)
   }
 
-  return res
+  return res, pos
 }
--- split_string_test.go
+++ split_string_test.go
@@ -13,10 +13,20 @@ func Test_SplitString(t *testing.T) {
   str, err := NewColorString(" \033[41;30mHey \033[1mhi\033[0;41;30m! \033[0m")
   assert.Nil(err)
 
-  parts := str.SplitString(" ")
+  parts, pos := str.SplitString(" ")
+
+  assert.Equal(4, len(parts))
+  assert.Equal(4, len(pos))
 
   assert.Equal("", parts[0].Join())
+  assert.Equal(uint(0), pos[0])
+
   assert.Equal("\033[41;30mHey", parts[1].Join())
+  assert.Equal(uint(1), pos[1])
+
   assert.Equal("\033[1mhi\033[0;41;30m!", parts[2].Join())
+  assert.Equal(uint(5), pos[2])
+
   assert.Equal("\033[0m", parts[3].Join())
+  assert.Equal(uint(9), pos[3])
 }
