c1b08e54ed0f8daf5da9d38917cfbdad6b584334
pandasoli <sadpandasoli@gmail.com>
2023-05-12 17:50:15 -0300

    feat: implement returning the removed part

--- remove.go
+++ remove.go
@@ -7,9 +7,9 @@ import (
 )
 
 
-func (self *ColorString) Remove(position, length uint) error {
+func (self *ColorString) Remove(position, length uint) (removed_part ColorString, err error) {
   if int(position) > len(self.string_) {
-    return fmt.Errorf("Cannot remove after the string's end")
+    return removed_part, fmt.Errorf("Cannot remove after the string's end")
   }
 
   // Never delete after the string's end
@@ -17,24 +17,27 @@ func (self *ColorString) Remove(position, length uint) error {
     length = uint(len(self.string_)) - position
   }
 
+  removed_part.string_ = self.string_[position:position + length]
   self.string_ = self.string_[:position] + self.string_[position + length:]
+
   var new_colors []Color
 
   for _, color := range self.colors {
     // Inside the range
     if position <= color.Position && color.Position < position + length {
-      continue
-    }
+      color.Position -= position
+      removed_part.colors = append(removed_part.colors, color)
+    } else {
+      // If it's from the right side we have to decrement the removed length
+      if position + length <= color.Position {
+        color.Position -= length
+      }
 
-    // At the right side
-    if position + length <= color.Position {
-      color.Position -= length
+      new_colors = append(new_colors, color)
     }
-
-    new_colors = append(new_colors, color)
   }
 
   self.colors = new_colors
 
-  return nil
+  return removed_part, err
 }
--- remove_test.go
+++ remove_test.go
@@ -13,9 +13,13 @@ func Test_Remove(t *testing.T) {
   str, err := NewColorString("\033[38;5;200mAB\033[2;40;31mC\033[0m")
   assert.Nil(err)
 
-  err = str.Remove(2, 1)
+  removed_part, err := str.Remove(2, 1)
   assert.Nil(err)
 
+  assert.Equal(
+    "\033[2;40;31mC",
+    removed_part.Join(),
+  )
   assert.Equal(
     "\033[38;5;200mAB\033[0m",
     str.Join(),
